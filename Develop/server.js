1a2043193041c01fe8d2caabb067ed465ab3139a
Stephanie Murphy <smurphy@mas.local>
2021-03-13 17:53:50 -0500

    updated port

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
